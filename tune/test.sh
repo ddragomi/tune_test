ff9c904fa6fff38efdc72c604e6d5677b64f76a5
Daniel Dragomir <daniel.dragomir@windriver.com>
2016-03-07 16:26:50 +0200

    Tune results with the patch against MASTER
    NOTE: I also added a new fake-machine (axxiaarm) that require arch-arm8a.inc
    to test/parse armv8a tunes:
    DEFAULTTUNE ?= "armv8a-neon"
    require conf/machine/include/arm/arch-armv8a.inc
    
    Signed-off-by: Daniel Dragomir <daniel.dragomir@windriver.com>

--- tune/test.sh
+++ tune/test.sh
@@ -8,7 +8,7 @@ export LC_ALL=C
 
 TUNE_TEST_DIR=`dirname $0`
 
-INCLUDES_TO_TEST=`find ../../openembedded-core/meta/conf/machine/include/ -name tune-\*.inc | sort | sed 's%openembedded-core/meta/%%g'`
+INCLUDES_TO_TEST=`find ../../openembedded-core_working/meta/conf/machine/include/ -name tune-\*.inc | sort | sed 's%openembedded-core_working/meta/%%g'`
 INCLUDES=`echo "${INCLUDES_TO_TEST}" | wc -l`
 INCLUDE=0
 
@@ -16,7 +16,7 @@ for I in ${INCLUDES_TO_TEST}; do
   M=`basename ${I} | sed 's/\.inc//g; s/^tune-/fake-/g'`
   INCLUDE=`expr ${INCLUDE} + 1`
   echo "Testing fake MACHINE ${M} for include ${I} (${INCLUDE}/${INCLUDES})"
-  echo "require ${I}" > ../../openembedded-core/meta/conf/machine/${M}.conf
+  echo "require ${I}" > ../../openembedded-core_working/meta/conf/machine/${M}.conf
   MACHINE=${M} bitbake -e openssl > ${TUNE_TEST_DIR}/log.${M} 2>&1
   grep "\(^export CC=\)\|\(^TUNE_CCARGS\)\|\(^TUNE_FEATURES=\)\|\(^PACKAGE_ARCHS=\)\|\(^TUNE_PKGARCH=\)\|\(^AVAILTUNES=\)" \
     ${TUNE_TEST_DIR}/log.${M} | sort |\
@@ -30,13 +30,14 @@ for I in ${INCLUDES_TO_TEST}; do
     rm -f ${TUNE_TEST_DIR}/log.${M}
   fi
   AVAILTUNES=`grep '^AVAILTUNES=' ${TUNE_TEST_DIR}/env.${M} | sed 's/^[^"]*"\([^"]*\)"/\1/g' | tr ' ' '\n'`;
-# AVAILTUNES=`grep '^AVAILTUNES=' ${TUNE_TEST_DIR}/env.${M} | sed 's/^[^"]*"\([^"]*\)"/\1/g' | tr ' ' '\n' | grep "\(neon-vfpv4\)"`;
+  # AVAILTUNES=`grep '^AVAILTUNES=' ${TUNE_TEST_DIR}/env.${M} | sed 's/^[^"]*"\([^"]*\)"/\1/g' | tr ' ' '\n' | grep "armv8a"`;
+  # AVAILTUNES=`grep '^AVAILTUNES=' ${TUNE_TEST_DIR}/env.${M} | sed 's/^[^"]*"\([^"]*\)"/\1/g' | tr ' ' '\n' | grep "armv7ahf-vfp-vfpv4-neon"`;
   TUNES=`echo "${AVAILTUNES}" | wc -l`
   TUNE=0
   for T in ${AVAILTUNES}; do
     TUNE=`expr ${TUNE} + 1`
     echo "Testing DEFAULTTUNE ${T} (${TUNE}/${TUNES}) for fake MACHINE ${M} (${INCLUDE}/${INCLUDES})"
-    echo "DEFAULTTUNE = \"${T}\"" >> ../../openembedded-core/meta/conf/machine/${M}.conf;
+    echo "DEFAULTTUNE = \"${T}\"" >> ../../openembedded-core_working/meta/conf/machine/${M}.conf;
     MACHINE=${M} bitbake -e openssl > ${TUNE_TEST_DIR}/log.${M}.${T} 2>&1
     grep "\(^export CC=\)\|\(^TUNE_CCARGS\)\|\(^TUNE_FEATURES=\)\|\(^PACKAGE_ARCHS=\)\|\(^TUNE_PKGARCH=\)" \
       ${TUNE_TEST_DIR}/log.${M}.${T} | sort |\
